d91fcae68e48a4fbd11401ac6e5fd3e1aa05426b
Inzimam <p126013@nu.edu.pk>
2017-05-05 22:31:35 +0500

    code refactoring

--- js/app.js
+++ js/app.js
@@ -1,38 +1,35 @@
+'use strict';
 // Generate random number between 2 numbers
 function randomNumber(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Check collisions between enemy & player
-function checkCollision(enemy, player) {
-    return (player.x > enemy.x - enemy.hitBox.x / 2 &&
-    player.x < enemy.x + enemy.hitBox.x / 2 &&
-    player.y > enemy.y - enemy.hitBox.y / 2 &&
-    player.y < enemy.y + enemy.hitBox.y / 2);
-}
-
 // Enemies our player avoid
 var Enemy = function (x, y, sprite) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Variables applied to each of our instances go here, we've provided one for
+    // you to get started
     this.x = x;
     this.y = y;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies, this uses a helper we've provided to easily
+    // load images
     this.sprite = 'images/enemy-bug.png';
     this.speed = randomNumber(300, 500);
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position, required method for game Parameter: dt, a time
+// delta between ticks
 Enemy.prototype.update = function (dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+    // Check collisions between enemy & player
+    function checkCollision(enemy, player) {
+        return (player.x > enemy.x - enemy.hitBox.x / 2 && player.x < enemy.x + enemy.hitBox.x / 2 && player.y > enemy.y - enemy.hitBox.y / 2 && player.y < enemy.y + enemy.hitBox.y / 2);
+    }
+
+    // You should multiply any movement by the dt parameter which will ensure the
+    // game runs at the same speed for all computers.
     if (this.x <= 505) {
         this.x += this.speed * dt;
-    }
-    else {
+    } else {
         this.x = -45;
         this.speed = randomNumber(300, 500);
     }
@@ -46,7 +43,10 @@ Enemy.prototype.update = function (dt) {
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-Enemy.prototype.hitBox = {'x': 101, 'y': 83};
+Enemy.prototype.hitBox = {
+    'x': 101,
+    'y': 83
+};
 
 // Now write your own player class
 var Player = function () {
@@ -60,20 +60,20 @@ Player.prototype.update = function () {
     var stepX = 101;
     var stepY = 83;
     switch (this.action) {
-        case 'up' :
+        case 'up':
             this.y -= stepY;
             break;
-        case 'down' :
+        case 'down':
             if (this.y <= 317) {
                 this.y += stepY;
             }
             break;
-        case 'left' :
+        case 'left':
             if (this.x >= 99) {
                 this.x -= stepX;
             }
             break;
-        case 'right' :
+        case 'right':
             if (this.x <= 301) {
                 this.x += stepX;
             }
@@ -102,15 +102,18 @@ Player.prototype.reset = function () {
     this.y = 400;
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [new Enemy(-100, 60), new Enemy(-75, 145), new Enemy(-50, 230)];
+// Now instantiate your objects. Place all enemy objects in an array called
+// allEnemies
+var allEnemies = [
+    new Enemy(-100, 60),
+    new Enemy(-75, 145),
+    new Enemy(-50, 230)
+];
 // Place the player object in a variable called player
 var player = new Player();
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to your Player.handleInput()
+// method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
